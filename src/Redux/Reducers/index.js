c69b47ba590dcc774daa32fa788a55de6a5d4849
aymenebdelli <aymenebdelli14@gmail.com>
2022-01-10 16:34:06 +0100

    first commit

---
+++ src/Redux/Reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from "redux"
+import tasksReducer from "./tasksReducer"
+//import usersReducers from "./usersReducers"
+
+const rootReducer = combineReducers({
+tasksReducer
+
+})
+
+export default rootReducer
